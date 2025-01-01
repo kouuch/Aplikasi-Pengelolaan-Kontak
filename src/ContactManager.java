637506b0be2d5dbde6e96a902837251ac08344b3
kouuch <rifkyirsada2@gmail.com>
2025-01-01 16:22:01 +0800

    menambah data kontak ke database

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,12 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-/**
- *
- * @author User
- */
 public class ContactManager {
-    
+
+    public static void addContact(String name, String phone, String address, String category) throws SQLException {
+        String sql = "INSERT INTO contacts (name, phone, address, category) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DatabaseHelper.connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, phone);
+            pstmt.setString(3, address);
+            pstmt.setString(4, category);
+            pstmt.executeUpdate();
+        }
+    }
 }
